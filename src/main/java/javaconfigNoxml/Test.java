abbd8831bb727f570d4715cb9d0cf5ebad0e9656
Ayushmaan Singh <rajputayushman119@gmail.com>
2025-08-15 23:29:03 +0530

    main

--- src/main/java/javaconfigNoxml/Test.java
+++ src/main/java/javaconfigNoxml/Test.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Test {
+
     public static void main(String[] args) {
         ApplicationContext context=  new AnnotationConfigApplicationContext(javaconfig.class);
         Student student = context.getBean("getStudent", Student.class);
